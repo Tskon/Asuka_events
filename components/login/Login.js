6c6c85060720253377ba3c7ecae181393927c804
Tskon <tsekhmeister.k@gmail.com>
2019-02-27 22:25:57 +0300

    solve eslint problems

--- components/login/Login.js
+++ components/login/Login.js
@@ -1,6 +1,41 @@
 import { Component } from 'react'
 
 export default class Header extends Component {
+  /**
+   * Не работает локально
+   * TODO допилить после выгрузки на сервер
+   */
+  static wgApiRequest() {
+    const url = 'https://api.worldoftanks.ru/wot/auth/login/?application_id=00dd20029028e3100534ec15656683cb&display=popup&redirect_uri=https%3A%2F%2Fdevelopers.wargaming.net%2Freference%2Fall%2Fwot%2Fauth%2Flogin%2F'
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      mode: 'cors',
+      body: 'foo=bar&lorem=ipsum',
+    })
+      .then(res => res.json())
+      .then((data) => {
+        console.log('Request succeeded with JSON response', data)
+      })
+      .catch((error) => {
+        console.log('Request failed', error)
+      })
+  }
+
+  static stopPropagation(e) {
+    e.stopPropagation()
+  }
+
+  static loginHandler() {
+    const { showModal } = this.state
+    this.setState({
+      showModal: !showModal,
+    })
+  }
+
   constructor(props) {
     super(props)
     this.state = {
@@ -10,54 +45,36 @@ export default class Header extends Component {
     this.loginHandler = this.loginHandler.bind(this)
   }
 
-  /**
-   * Не работает локально
-   * TODO допилить после выгрузки на сервер
-   */
-  wgApiRequest() {
-    const url = 'https://api.worldoftanks.ru/wot/auth/login/?application_id=00dd20029028e3100534ec15656683cb&display=popup&redirect_uri=https%3A%2F%2Fdevelopers.wargaming.net%2Freference%2Fall%2Fwot%2Fauth%2Flogin%2F'
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      mode: 'cors',
-      body: 'foo=bar&lorem=ipsum'
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log('Request succeeded with JSON response', data);
-      })
-      .catch(error => {
-        console.log('Request failed', error);
-      });
-  }
-
-  stopPropagation(e) {
-    e.stopPropagation()
-  }
-
-  loginHandler(e) {
-    this.setState({
-      showModal: !this.state.showModal
-    })
-  }
-
   render() {
+    const { showModal } = this.state
+
     const modal = (
-      <div className="login__modal-shadow" onClick={this.loginHandler}>
+      <div role="button" tabIndex={0} className="login__modal-shadow" onClick={this.loginHandler}>
         <div className="login__modal" onClick={this.stopPropagation}>
-          <button className="login__modal-close-btn"
-                  onClick={this.loginHandler}>
+          <button
+            className="login__modal-close-btn"
+            onClick={this.loginHandler}
+          >
+
+
             &#10006;
           </button>
           <form action="/login" method="POST">
             <div className="login__field">
-              <label>Login: <input type="text"/></label>
+              <label>
+
+
+Login:
+                <input type="text" />
+              </label>
             </div>
             <div className="login__field">
-              <label>Pass: <input type="password"/></label>
+              <label>
+
+
+Pass:
+                <input type="password" />
+              </label>
             </div>
             <button className="login__submit-btn" type="submit">Login</button>
           </form>
@@ -70,7 +87,7 @@ export default class Header extends Component {
         <div className="login__button" onClick={this.loginHandler}>
           Login
         </div>
-        {this.state.showModal && modal}
+        {showModal && modal}
 
       </div>
     )
--- pages/forum/index.js
+++ pages/forum/index.js
@@ -1,11 +1,5 @@
-import {Component} from 'react'
-
-export default class Forum extends Component{
-  render(){
-    return (
-      <main>
-        <h1 className="example">Forum</h1>
-      </main>
-    )
-  }
-}
+export default () => (
+  <main>
+    <h1 className="example">Forum</h1>
+  </main>
+)
