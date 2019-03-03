d8fbcf5ce51ac1ba375970254da9b4d124ad51d6
Tskon <tsekhmeister.k@gmail.com>
2019-03-03 16:13:36 +0300

    del waste props

--- components/menu/MainMenu.js
+++ components/menu/MainMenu.js
@@ -2,8 +2,8 @@ import Link from '../ui/Link'
 
 export default () => (
   <nav className="menu">
-    <Link activeClassName="active" href="/"><a>Home</a></Link>
-    <Link activeClassName="active" href="/about"><a>About</a></Link>
-    <Link activeClassName="active" href="/forum"><a>Forum</a></Link>
+    <Link href="/"><a>Home</a></Link>
+    <Link href="/about"><a>About</a></Link>
+    <Link href="/forum"><a>Forum</a></Link>
   </nav>
 )
