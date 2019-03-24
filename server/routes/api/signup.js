e3fcc7fc88ed92c5f3eb49f822d4bc1aeb424084
tskon <tsekhmeister.k@gmail.com>
2019-03-24 17:52:14 +0300

    user registration complete

--- server/routes/api/signup.js
+++ server/routes/api/signup.js
@@ -1,4 +1,4 @@
-module.exports = function (router, modules, passport) {
+module.exports = function (router, models, passport) {
   // router.post('/signup', passport.authenticate('local-signup', {
   //   successRedirect: '/success-signup',
   //   failureRedirect: '/fail-signup',
@@ -6,13 +6,20 @@ module.exports = function (router, modules, passport) {
   router.post('/signup', (req, res) => {
     // TODO переделать поиск по введенным данным, а не всех пользователей
     console.log('req body: ', req.body)
-    res.end('hi from signup')
-    modules.user.findAll()
+
+    models.user.findAll()
       .then((answer) => {
         // Если пользователя нет, то создаем нового
+        console.log('====== num of users ======',answer.length)
         if (answer.length === 0) {
+          models.user.create({
+            username: req.body.login,
+            password: req.body.password,
+          })
 
+          res.end('registration success')
         }
+        res.end('registration failed')
       })
   })
 }
