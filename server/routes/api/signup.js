a88aade988811df667e9bfddf356d16db3bf3779
tskon <tsekhmeister.k@gmail.com>
2019-03-24 23:13:22 +0300

    add TODO

--- server/routes/api/signup.js
+++ server/routes/api/signup.js
@@ -2,3 +2,4 @@ module.exports = function (router, models, passport) {
   models.sequelize.sync()
   router.post('/signup', passport.authenticate('local-signup'))
 }
+// TODO решить проблему POST http://localhost:3000/api/signup 404 (Not Found) в ответе: Cannot POST /api/signup
