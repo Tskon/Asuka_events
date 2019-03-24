abf9467a0c83b5eca9a38692f6d48edc00f8ac44
tskon <tsekhmeister.k@gmail.com>
2019-03-24 22:15:58 +0300

    del waste code

--- components/login/Login.js
+++ components/login/Login.js
@@ -3,30 +3,6 @@ import TextInput from '../ui/TextInput'
 import CheckboxLoginTypeSwitcher from './CheckboxLoginTypeSwitcher'
 
 export default class Login extends Component {
-  /**
-   * Не работает локально
-   * TODO допилить после выгрузки на сервер
-   */
-  static wgApiRequest() {
-    const url = 'https://api.worldoftanks.ru/wot/auth/login/?application_id=00dd20029028e3100534ec15656683cb&display=popup&redirect_uri=https%3A%2F%2Fdevelopers.wargaming.net%2Freference%2Fall%2Fwot%2Fauth%2Flogin%2F'
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      mode: 'cors',
-      body: 'foo=bar&lorem=ipsum',
-    })
-      .then(res => res.json())
-      .then((data) => {
-        console.log('Request succeeded with JSON response', data)
-      })
-      .catch((error) => {
-        console.log('Request failed', error)
-      })
-  }
-
   static stopPropagation(e) {
     e.stopPropagation()
   }
