c4c005d92346d074ab0e6b3fba1c6ccd134d8a7b
tskon <tsekhmeister.k@gmail.com>
2019-03-24 22:19:03 +0300

    del waste comment

--- server/routes/api/signup.js
+++ server/routes/api/signup.js
@@ -1,5 +1,4 @@
 module.exports = function (router, models, passport) {
-  // TODO решить проблему с Error: Failed to serialize user into session
   router.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/success',
     failureRedirect: '/failure',
