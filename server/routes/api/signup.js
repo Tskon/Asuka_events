90f79cae743350ab54b12345b423195ee70aeb43
tskon <tsekhmeister.k@gmail.com>
2019-03-24 19:21:45 +0300

    add sync for user model

--- server/routes/api/signup.js
+++ server/routes/api/signup.js
@@ -7,20 +7,25 @@ module.exports = function (router, models, passport) {
     // TODO переделать поиск по введенным данным, а не всех пользователей
     console.log('req body: ', req.body)
 
-    models.user.findOne({
-      where: { username: req.body.login },
-    })
-      .then((answer) => {
-        // Если пользователя нет, то создаем нового
-        if (answer === null) {
-          models.user.create({
-            username: req.body.login,
-            password: req.body.password,
-          })
+    models.sequelize
+      .sync()
+      .then(() => {
+        models.user.findOne({
+          where: { username: req.body.login },
+        })
+          .then((answer) => {
+            // Если пользователя нет, то создаем нового
+            if (answer === null) {
+              models.user.create({
+                username: req.body.login,
+                password: req.body.password,
+              })
 
-          res.end('registration success')
-        }
-        res.end('registration failed')
+              res.end('registration success')
+            }
+            res.end('registration failed')
+          })
       })
+
   })
 }
