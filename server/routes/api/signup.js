98b2b0edfb7576f8a679c5f321f1ba9057becf39
tskon <tsekhmeister.k@gmail.com>
2019-03-24 18:29:38 +0300

    upgrade check-user method

--- server/routes/api/signup.js
+++ server/routes/api/signup.js
@@ -7,11 +7,12 @@ module.exports = function (router, models, passport) {
     // TODO переделать поиск по введенным данным, а не всех пользователей
     console.log('req body: ', req.body)
 
-    models.user.findAll()
+    models.user.findOne({
+      where: { username: req.body.login },
+    })
       .then((answer) => {
         // Если пользователя нет, то создаем нового
-        console.log('====== num of users ======',answer.length)
-        if (answer.length === 0) {
+        if (answer === null) {
           models.user.create({
             username: req.body.login,
             password: req.body.password,
