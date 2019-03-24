65f4fe395e7b59925d44b20288c22bb109ac83af
tskon <tsekhmeister.k@gmail.com>
2019-03-24 22:18:33 +0300

    del waste code

--- server/routes/api/signup.js
+++ server/routes/api/signup.js
@@ -5,24 +5,4 @@ module.exports = function (router, models, passport) {
     failureRedirect: '/failure',
     failureFlash: true,
   }))
-
-  // models.sequelize
-  //   .sync()
-  //   .then(() => {
-  //     models.user.findOne({
-  //       where: { username: req.body.username },
-  //     })
-  //       .then((answer) => {
-  //         // Если пользователя нет, то создаем нового
-  //         if (answer === null) {
-  //           models.user.create({
-  //             username: req.body.username,
-  //             password: req.body.password,
-  //           })
-  //
-  //           res.end('registration success')
-  //         }
-  //         res.end('registration failed')
-  //       })
-  //   })
 }
