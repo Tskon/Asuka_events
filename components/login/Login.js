052da62425dabecdaabe67e4b8478b62170a5a25
tskon <tsekhmeister.k@gmail.com>
2019-03-24 22:14:24 +0300

    add signin request

--- components/login/Login.js
+++ components/login/Login.js
@@ -95,7 +95,7 @@ export default class Login extends Component {
 
     try {
       const url = (currentType === 'signin') ? '/api/signin' : '/api/signup'
-      fetch('/api/signup', myInit)
+      fetch(url, myInit)
         .then(response => response.text())
         .then((data) => {
           console.log(data)
