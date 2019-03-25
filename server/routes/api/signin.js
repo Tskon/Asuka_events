79356f6456d917ebfd08de75fd8390fc94cf54d1
tskon <tsekhmeister.k@gmail.com>
2019-03-25 19:45:43 +0300

    add data response on signIn

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -1,6 +1,12 @@
 module.exports = function (router, models, passport) {
   models.sequelize.sync()
   router.post('/signin', passport.authenticate('local-signin'), (req, res) => {
-    res.send('signin is ok')
+    res.send({
+      message: 'signin is ok',
+      data: {
+        name: req.user.username,
+        id: req.user.id,
+      },
+    })
   })
 }
