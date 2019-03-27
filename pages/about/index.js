82cf59650cf97c44710160faff9368e84a2bccba
tskon <tsekhmeister.k@gmail.com>
2019-03-27 22:19:43 +0300

    map content

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,33 @@
-export default () => (
-  <main>
-    <h1 className="event-header">WoWs amateur`s event</h1>
-    <div className="event-map" />
-  </main>
-)
+import { Component } from 'react'
+
+export default class About extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      blocsNumInRow: 20,
+      blocsNumInCol: 20,
+    }
+
+    this.gridElems = []
+    for (let i = 0; i < this.state.blocsNumInRow * this.state.blocsNumInCol; i += 1) {
+      this.gridElems.push(
+        <div
+          className="event-map__elem"
+          key={`map-elem-${i}`}
+        />,
+      )
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <h1 className="event-header">WoWs amateur`s event</h1>
+        <div className="event-map">
+          { this.gridElems }
+        </div>
+      </main>
+    )
+  }
+}
