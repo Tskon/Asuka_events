403fa2322c226431ce66b8785d583aff139b6e22
tskon <tsekhmeister.k@gmail.com>
2019-03-30 20:03:36 +0300

    fix active menu

--- src/components/menu/MainMenu.js
+++ src/components/menu/MainMenu.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-// import Link from '../ui/Link'
+
+const isActiveChecker = (match, location) => {
+  if (!match) return false
+  return match.url === location.pathname
+}
 
 export default () => (
   <nav className="menu">
-    <NavLink to="/" activeClassName="active">Home</NavLink>
-    <NavLink to="/about" activeClassName="active">About</NavLink>
-    <NavLink to="/forum" activeClassName="active">Forum</NavLink>
+    <NavLink to="/" activeClassName="active" isActive={isActiveChecker}>Home</NavLink>
+    <NavLink to="/event" activeClassName="active" isActive={isActiveChecker}>Event</NavLink>
   </nav>
 )
--- src/layout/main.js
+++ src/layout/main.js
@@ -4,16 +4,14 @@ import { Route } from 'react-router'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import IndexPage from '../pages/index'
-import AboutPage from '../pages/about/index'
-import ForumPage from '../pages/forum/index'
+import AboutPage from '../pages/event/index'
 
 export default () => (
   <div>
     <Header />
     <main>
       <Route exact path="/" component={IndexPage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/forum" component={ForumPage} />
+      <Route path="/event" component={AboutPage} />
     </main>
     <Footer />
   </div>
