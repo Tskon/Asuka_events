0e244c43244574ea37f6cfd52d6f8b64dcfa3b89
tskon <tsekhmeister.k@gmail.com>
2019-03-30 12:38:01 +0300

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,23 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './app.js',
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
 }
