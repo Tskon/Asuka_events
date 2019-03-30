4d7e81c65d3028b258d0d2c753203c550a10e410
tskon <tsekhmeister.k@gmail.com>
2019-03-30 11:47:58 +0300

    add layout

---
+++ layout/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export default () => (
+  <div>
+    <Header />
+    <main>
+        Content
+    </main>
+    <Footer />
+  </div>
+)
