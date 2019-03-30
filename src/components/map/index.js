0057676bf11015a0afb05e3514774ea83718e010
tskon <tsekhmeister.k@gmail.com>
2019-03-30 19:53:55 +0300

    separate map

---
+++ src/components/map/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../../scss/map/map.scss'
+
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      blocsNumInRow: 8,
+      blocsNumInCol: 6,
+    }
+
+    this.gridElems = []
+    for (let i = 0; i < this.state.blocsNumInRow * this.state.blocsNumInCol; i += 1) {
+      this.gridElems.push(
+        <div
+          className="event-map__elem"
+          key={`map-elem-${i}`}
+        />,
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="event-map">
+        {this.gridElems}
+      </div>
+    )
+  }
+}
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,33 +1,19 @@
 import React from 'react'
 import '../../scss/pages/about.scss'
 
+import Map from '../../components/map/index'
+
 export default class About extends React.Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      blocsNumInRow: 8,
-      blocsNumInCol: 6,
-    }
-
-    this.gridElems = []
-    for (let i = 0; i < this.state.blocsNumInRow * this.state.blocsNumInCol; i += 1) {
-      this.gridElems.push(
-        <div
-          className="event-map__elem"
-          key={`map-elem-${i}`}
-        />,
-      )
-    }
+    this.state = {}
   }
 
   render() {
     return (
       <div className="event-wrapper">
         <h1 className="event-header">WoWs amateur`s event</h1>
-        <div className="event-map">
-          { this.gridElems }
-        </div>
+        <Map />
       </div>
     )
   }
