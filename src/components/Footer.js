4f7b688448af34d57c46ea1c76c9e3a58a371148
tskon <tsekhmeister.k@gmail.com>
2019-04-15 22:31:44 +0300

    footer`s comeback

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 export default () => (
   <footer className="footer">
-    {/*<p>*/}
-      {/*© Wargaming.net.*/}
-      {/*Все права защищены. Источником игровых данных*/}
-      {/*пользователя является*/}
-      {/*{' '}*/}
-      {/*<a href="https://worldofwarships.ru/">Wargaming</a>*/}
-    {/*</p>*/}
-    {/*<a*/}
-      {/*target="_blank"*/}
-      {/*rel="noopener noreferrer"*/}
-      {/*href="https://ru.wargaming.net/support/ru/products/wows/"*/}
-    {/*>*/}
-      {/*Центр поддержки пользователей WoWs*/}
-    {/*</a>*/}
+    <p>
+      © Wargaming.net.
+      Все права защищены. Источником игровых данных
+      пользователя является
+      {' '}
+      <a href="https://worldofwarships.ru/">Wargaming</a>
+    </p>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://ru.wargaming.net/support/ru/products/wows/"
+    >
+      Центр поддержки пользователей WoWs
+    </a>
   </footer>
 )
