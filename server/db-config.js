01fe4ab054474401593f33fa3c70ccbdf1dceb01
tskon <tsekhmeister.k@gmail.com>
2019-04-28 11:14:32 +0300

    remove hostname

--- server/db-config.js
+++ server/db-config.js
@@ -3,7 +3,6 @@ module.exports = {
   dbUser: 'tskon1_asuka_frm',
   dbPassword: 'q1w2Q!W@',
   dbOptions: {
-    // host: 'tskon1.beget.tech',
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
