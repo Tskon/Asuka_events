291753e71a796951ec926ca3f2d9e7cc49696c42
tskon <tsekhmeister.k@gmail.com>
2019-04-29 14:48:22 +0300

    del waste log

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 3000
 function mustAuthenticated(req, res, next) {
   console.log('=========== check auth ============')
   console.log(req.isAuthenticated(), req.user)
-  console.log(req.cookies)
   console.log('=========== fin ============')
   next()
 }
