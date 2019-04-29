2168c584315d4d0c19bb3ae9bf9c2873725e6e18
tskon <tsekhmeister.k@gmail.com>
2019-04-29 14:01:07 +0300

    remove redis repair sessions. RIP when server was restart

--- server/index.js
+++ server/index.js
@@ -6,48 +6,49 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
 const models = require('./models/index')
-const RedisStore = require('connect-redis')(express);
 
 const PORT = process.env.PORT || 3000
 
+
+// test function.
+function mustAuthenticated(req, res, next) {
+  console.log('=========== check auth ============')
+  console.log(req.isAuthenticated(), req.user)
+  console.log(req.cookies)
+  console.log('=========== fin ============')
+  next()
+}
+
 const app = express()
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser());
-app.use(session({
-  store: new RedisStore(),
-  resave: true,
-  rolling: true,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 10 * 60 * 1000,
-    httpOnly: false,
-  },
-  secret: 'asuka_and_ray',
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(cookieParser())
+  .use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 20 },
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
 
-// Serve the static files from the React app
-app.use(express.static(path.resolve(__dirname, '../dist')))
+  // Serve the static files from the React app
+  .use(express.static(path.resolve(__dirname, '../dist')))
 
-/**
- * Routes. use path /api + path in routes.
- * For example: /api/auth
- */
-app.use('/api', require('./routes/api/index')(passport))
+  /**
+   * Routes. use path /api + path in routes.
+   * For example: /api/auth
+   */
+  .use('/api', require('./routes/api/index')(passport))
 
-/**
- * for all the react stuff
- */
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../dist/index.html'))
-})
-
-app.post('*', (req, res) => {
-  res.send({ status: 'failed!' })
-})
+  /**
+   * for all the react stuff
+   */
+  .use(mustAuthenticated)
+  .get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+  })
 
 /**
  * Load passport strategies
