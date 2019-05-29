0f1761af71ef1bb79deacb182183522b448095c6
kostya <tsekhmeister.k@gmail.com>
2019-05-29 15:57:31 +0300

    add light theme for content

--- src/layout/main.js
+++ src/layout/main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Route } from 'react-router'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import IndexPage from '../pages/index'
@@ -9,15 +10,25 @@ import AdminPage from '../pages/admin/index'
 import LKPage from '../pages/lk/index'
 import Snackbar from '../components/ui/Snackbar'
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    // primary: { main: '#fdc073' },
+    // secondary: { main: '#498161' },
+  },
+})
+
 export default () => (
   <div>
     <Header />
-    <main className='site-content'>
-      <Route exact path="/" component={IndexPage} />
-      <Route path="/event" component={AboutPage} />
-      <Route path="/admin" component={AdminPage} />
-      <Route path="/lk" component={LKPage} />
-    </main>
+    <MuiThemeProvider theme={theme}>
+      <main className='site-content'>
+        <Route exact path="/" component={IndexPage} />
+        <Route path="/event" component={AboutPage} />
+        <Route path="/admin" component={AdminPage} />
+        <Route path="/lk" component={LKPage} />
+      </main>
+    </MuiThemeProvider>
     <Footer />
     <Snackbar />
   </div>
