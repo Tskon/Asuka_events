39886cec6f10d22ab7cbf32d0054be509f705e4e
tskon <tsekhmeister.k@gmail.com>
2019-06-11 21:40:53 +0300

    additional fields for LK

--- src/pages/lk/LkView.js
+++ src/pages/lk/LkView.js
@@ -9,7 +9,19 @@ const LkView = props => (
     <h1>Личный кабинет</h1>
     <form className="lk-form" onSubmit={props.submitHandler}>
       <TextField
-        label="Ссылка на изображение"
+        label="Название клана"
+        name="clanName"
+        type="text"
+        onChange={props.inputHandler}
+      />
+      <TextField
+        label="Клан-тег"
+        name="clanTag"
+        type="text"
+        onChange={props.inputHandler}
+      />
+      <TextField
+        label="Ссылка на аватар"
         name="image"
         type="text"
         autoComplete="username"
