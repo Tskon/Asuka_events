5c102675c67aaa73f7ebab272c659d1eb2a17a2b
tskon <tsekhmeister.k@gmail.com>
2019-07-06 12:57:52 +0300

    del waste console log

--- server/routes/api/logout.js
+++ server/routes/api/logout.js
@@ -1,6 +1,5 @@
 module.exports = function (router, models, passport) {
   router.post('/logout', (req, res) => {
-    console.log('== logout ==', req.user)
     req.logout()
     res.send({ status: 'ok' })
   })
