73479483736a35b0ae0984505c6fa9be1f428f76
tskon <tsekhmeister.k@gmail.com>
2019-07-07 11:42:51 +0300

    change theme

--- src/components/map/actionBar/ActionBar.js
+++ src/components/map/actionBar/ActionBar.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import Drawer from '@material-ui/core/Drawer'
 import PropTypes from 'prop-types'
+import {createMuiTheme} from "@material-ui/core"
+import {MuiThemeProvider} from "@material-ui/core/styles"
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Roboto',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+    useNextVariants: true,
+  },
+  palette: {
+    type: 'dark',
+    primary: { main: '#fdc073' },
+    secondary: { main: '#498161' },
+  },
+})
 
 export default class ActionBar extends React.Component {
   static propTypes = {
@@ -18,13 +39,15 @@ export default class ActionBar extends React.Component {
 
   render() {
     return (
-      <Drawer
-        open={this.props.show}
-        onClose={this.props.closeHandler}
-        anchor="right"
-      >
-        lorem asfdasd asdas dad
-      </Drawer>
+      <MuiThemeProvider theme={theme}>
+        <Drawer
+          open={this.props.show}
+          onClose={this.props.closeHandler}
+          anchor="right"
+        >
+          lorem asfdasd asdas dad
+        </Drawer>
+      </MuiThemeProvider>
     )
   }
 }
