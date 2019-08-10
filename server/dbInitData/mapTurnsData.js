facdfd52f42c9a594bda824b2375acacfc06e29e
tskon <tsekhmeister.k@gmail.com>
2019-08-10 20:06:01 +0300

    add fix (duplication of turns)

--- server/dbInitData/mapTurnsData.js
+++ server/dbInitData/mapTurnsData.js
@@ -4,12 +4,19 @@ module.exports = (models) => {
     {name: 'commonTurn', count: 8, fog: true}
   ]
 
-  turns.forEach(turn => {
-    for (let i = 0; i < turn.count; i++) {
-      models.mapTurnData.create({
-        turnName: turn.name,
-        fog: turn.fog
-      })
-    }
+  models.mapTurnData.findAll({
+    limit: 1
   })
+    .then((turnsInDb => {
+      if (turnsInDb[0]) return
+
+      turns.forEach(turn => {
+        for (let i = 0; i < turn.count; i++) {
+          models.mapTurnData.create({
+            turnName: turn.name,
+            fog: turn.fog
+          })
+        }
+      })
+    }))
 }
