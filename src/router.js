69f1fdaf4f1080211f05283ac3e71c833dbfe342
tskon <tsekhmeister.k@gmail.com>
2019-09-08 14:02:48 +0300

    add routes

--- src/router.js
+++ src/router.js
@@ -12,12 +12,19 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/event',
+      name: 'event',
+      component: () => import(/* webpackChunkName: "event" */ './views/About.vue')
+    },
+    {
+      path: '/personalPanel',
+      name: 'personalPanel',
+      component: () => import(/* webpackChunkName: "personalPanel" */ './views/About.vue')
+    },
+    {
+      path: '/adminPanel',
+      name: 'adminPanel',
+      component: () => import(/* webpackChunkName: "adminPanel" */ './views/About.vue')
     }
   ]
 })
