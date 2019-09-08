e85a342cc8d9286d4bc7e9204820aad259a0b5e7
tskon <tsekhmeister.k@gmail.com>
2019-09-08 15:11:52 +0300

    add main menu

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <router-link to="/">Главная</router-link> |
+    <router-link to="/event">Эвент</router-link> |
+    <router-link to="/event">Личный кабинет</router-link> |
+    <router-link to="/event">Админка</router-link>
+  </nav>
+</template>
