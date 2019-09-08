3d904a1c5ce024862021f16470f981eba090d798
tskon <tsekhmeister.k@gmail.com>
2019-09-08 23:21:30 +0300

    cfg

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,10 @@
 const path = require('path')
 module.exports = {
+  runtimeCompiler: true,
+  // configureWebpack: {},
   pluginOptions: {
     'style-resources-loader': {
       'patterns': [ path.resolve(__dirname, 'src/assets/scss/variables') ]
     }
   }
-}
-
-module.exports = {
-  runtimeCompiler: true
 }
