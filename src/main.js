9be3353e29a0eed8521c936f77f258c003870568
tskon <tsekhmeister.k@gmail.com>
2019-09-09 20:47:49 +0300

    add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import { LayoutPlugin, ModalPlugin, DropdownPlugin, TablePlugin, CardPlugin } from 'bootstrap-vue'
+import './assets/scss/vendor/bootstrap.scss'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+Vue.use(LayoutPlugin)
+Vue.use(ModalPlugin)
+Vue.use(CardPlugin)
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
 
 Vue.mixin({
   data() {
