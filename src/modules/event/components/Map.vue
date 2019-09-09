72ba8336385ae6ca702f9bad5d80eae95178966d
tskon <tsekhmeister.k@gmail.com>
2019-09-09 21:23:35 +0300

    add map

---
+++ src/modules/event/components/Map.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import Cell
+
+  export default {
+    data() {
+      return {
+        cols: 8,
+        rows: 6,
+        selectedCell: 'a1',
+        letters: 'abcdefghijklmnop'
+      }
+    },
+
+    computed: {
+      cells() {
+        const cells = []
+
+        for (let i = 0; i < this.state.cols * this.state.rows; i += 1) {
+          const rowNum = Math.ceil((i + 1) / this.state.cols)
+          const colNum = (i + 1) % this.state.cols || this.state.cols
+          const colLetter = this.letters[colNum - 1]
+          cells.push(colLetter + rowNum)
+        }
+
+        return cells
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
