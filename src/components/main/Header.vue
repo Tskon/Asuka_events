8ac4074eefb9ae1f5a5139d50baf36fc99263cb6
tskon <tsekhmeister.k@gmail.com>
2019-09-14 00:19:07 +0300

    add login modal

--- src/components/main/Header.vue
+++ src/components/main/Header.vue
@@ -1,7 +1,40 @@
 <template>
   <header>
     <div class="d-flex">
-      <b-button>Login</b-button>
+      <b-button v-b-modal.login-modal>
+        Войти
+      </b-button>
+      <b-modal
+        id="login-modal"
+        title="BootstrapVue"
+      >
+        <b-form-group
+          label="Email address:"
+          label-for="login-input"
+          description="Не менее 6 символов"
+        >
+          <b-form-input
+            id="login-input"
+            v-model="form.login"
+            type="email"
+            required
+            placeholder="Enter email"
+          />
+        </b-form-group>
+
+        <b-form-group
+          label="Password:"
+          label-for="password-input"
+          description="Не менее 6 символов"
+        >
+          <b-form-input
+            id="password-input"
+            v-model="form.password"
+            required
+            placeholder="Enter name"
+          />
+        </b-form-group>
+      </b-modal>
     </div>
 
     <MainMenu/>
@@ -10,11 +43,24 @@
 
 <script>
 import MainMenu from "./MainMenu"
+
 export default {
   components: {
     MainMenu
   },
+  data () {
+    return {
+      form: {
+        login: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
 
+    }
+  }
 }
 </script>
 
