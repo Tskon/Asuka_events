3533b009e794a6f437eccc7527f86400ad0b9d7f
tskon <tsekhmeister.k@gmail.com>
2019-09-14 23:35:13 +0300

    add check to login

--- src/components/main/Header.vue
+++ src/components/main/Header.vue
@@ -1,42 +1,50 @@
 <template>
   <header>
     <div class="d-flex">
-      <b-button v-b-modal.login-modal>
-        Войти
-      </b-button>
-      <b-modal
-        id="login-modal"
-        title="Войти"
-        @ok="onSubmit"
-        @hidden="resetForm"
-      >
-        <b-form-group
-          label="Email address:"
-          label-for="login-input"
-          description="Не менее 6 символов"
+      <template v-if="!user.id">
+        <b-button v-b-modal.login-modal>
+          Войти
+        </b-button>
+        <b-modal
+          id="login-modal"
+          title="Войти"
+          @ok="onSubmit"
+          @hidden="resetForm"
         >
-          <b-form-input
-            id="login-input"
-            v-model="form.username"
-            type="email"
-            required
-            placeholder="Enter email"
-          />
-        </b-form-group>
+          <b-form-group
+            label="Email address:"
+            label-for="login-input"
+            description="Не менее 6 символов"
+          >
+            <b-form-input
+              id="login-input"
+              v-model="form.username"
+              type="email"
+              required
+              placeholder="Enter email"
+            />
+          </b-form-group>
 
-        <b-form-group
-          label="Password:"
-          label-for="password-input"
-          description="Не менее 6 символов"
-        >
-          <b-form-input
-            id="password-input"
-            v-model="form.password"
-            required
-            placeholder="Enter name"
-          />
-        </b-form-group>
-      </b-modal>
+          <b-form-group
+            label="Password:"
+            label-for="password-input"
+            description="Не менее 6 символов"
+          >
+            <b-form-input
+              id="password-input"
+              v-model="form.password"
+              required
+              placeholder="Enter name"
+            />
+          </b-form-group>
+        </b-modal>
+      </template>
+      <div
+        v-else
+        class="info"
+      >
+        {{user.name}}
+      </div>
     </div>
 
     <MainMenu/>
@@ -44,7 +52,7 @@
 </template>
 
 <script>
-import {mapActions} from 'vuex'
+import {mapActions, mapState} from 'vuex'
 import MainMenu from './MainMenu'
 
 export default {
@@ -58,6 +66,11 @@ export default {
         password: ''
       }
     }
+  },
+  computed: {
+    ...mapState({
+      user: state => state.user.user
+    })
   },
   methods: {
     ...mapActions({getUser: 'user/getUser'}),
