3742ca79b9ff31532f7194a8045c439793647bd3
tskon <tsekhmeister.k@gmail.com>
2019-10-06 17:46:48 +0300

    add smart choose sector for player

--- server/routes/api/map/next-turn.js
+++ server/routes/api/map/next-turn.js
@@ -15,7 +15,9 @@ module.exports = function (router, models) {
         models.mapLog.create({playersJson, cellsJson})
 
         res.send({
-          status: 'ok'
+          status: 'ok',
+          pl: JSON.parse(playersJson),
+          map: JSON.parse(cellsJson)
         })
       })
 
@@ -23,15 +25,47 @@ module.exports = function (router, models) {
 }
 
 function getJson (mapData, usersData) {
+  console.log(mapData, usersData)
+
+  const parcedMapData = mapData.map(cell => {
+
+    return {
+      cellName: cell.cellName,
+      ...JSON.parse(cell.dataJson)
+    }
+  })
+
+  const parcedUsersData = usersData.map(user => {
+    smartSectorChoose (user, parcedMapData)
+
+    user.cellId = ''
+    return user
+  })
+
   return {
-    playersJson: JSON.stringify(usersData),
-    cellsJson: JSON.stringify(
-      mapData.map(cell => {
-        return {
-          cellName: cell.cellName,
-          ...JSON.parse(cell.dataJson)
-        }
-      })
-    )
+    playersJson: JSON.stringify(parcedUsersData),
+    cellsJson: JSON.stringify(parcedMapData)
+  }
+}
+
+function smartSectorChoose (player, mapData) {
+  if (player.cellId) {
+    mapData.find(cell => {
+      if (cell.cellName === player.cellId) {
+        cell.players.push(player.userId)
+        return true
+      }
+
+      return false
+    })
+  } else {
+    const startedSectors = mapData.filter(cell => cell.isStarted)
+
+    const cellWithPlayer = mapData.find(cell => cell.players.includes(player.userId))
+
+    if (!cellWithPlayer) {
+      const randomSector = startedSectors[Math.floor(Math.random()*startedSectors.length)]
+      randomSector.players.push(player.userId)
+    }
   }
 }
