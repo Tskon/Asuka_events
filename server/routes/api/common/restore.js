3be66b5c55b5d39d307716bfd038a84407bcd7a1
tskon <tsekhmeister.k@gmail.com>
2019-10-16 22:36:31 +0300

    add restore password api

---
+++ server/routes/api/common/restore.js
@@ -0,0 +1,22 @@
+const bCrypt = require('bcrypt-nodejs')
+const generateHash = password => bCrypt.hashSync(password, bCrypt.genSaltSync(8), null)
+
+module.exports = function (router, models, passport) {
+  router.post('/restore', (req, res) => {
+    models.user.findOne({
+      where: {username: req.body.username}
+    }).then((data) => {
+      if (bCrypt.compareSync(req.body.secret, data.secret)) {
+        models.user.update({
+          password: generateHash(req.body.newPassword)
+        }).where({id: data.id})
+
+        res.send({
+          status: 'ok'
+        })
+      } else {
+        res.send({status: 'error', message: 'Неправильное секретное слово'})
+      }
+    })
+  })
+}
