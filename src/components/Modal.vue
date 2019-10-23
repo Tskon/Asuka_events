4645214a3f2742e09ab079e2477121a553f2dcdd
tskon <tsekhmeister.k@gmail.com>
2019-10-23 22:10:40 +0300

    fix err

---
+++ src/components/Modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    {{ body }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed:
+}
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+  }
+</style>
