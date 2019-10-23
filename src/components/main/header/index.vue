1cf795b3e97e86d6d12650a51189567c19c48b6f
tskon <tsekhmeister.k@gmail.com>
2019-10-23 22:10:15 +0300

    add message/modal to layout

--- src/components/main/header/index.vue
+++ src/components/main/header/index.vue
@@ -1,65 +1,73 @@
 <template>
-  <b-navbar
-    toggleable="sm"
-    type="dark"
-    variant="dark"
-    class="row"
-  >
-    <b-navbar-brand to="/">
-      Asuka
-    </b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"/>
-    <b-collapse
-      id="nav-collapse"
-      is-nav
+  <div>
+    <b-navbar
+      toggleable="sm"
+      type="dark"
+      variant="dark"
+      class="row"
     >
-      <MainMenu/>
-      <b-navbar-nav class="ml-auto">
-        <Login
-          v-if="!user.id"
-          class="mr-1"
-        />
-        <Restore
-          v-if="!user.id"
-          class="mr-1"
-        />
-        <SignUp v-if="!user.id"/>
-        <b-nav-item-dropdown
-          v-else
-          right
-        >
-          <template slot="button-content">
-            {{ user.name }}
-          </template>
-          <b-dropdown-text v-if="user.isAdmin">
-            Администратор
-          </b-dropdown-text>
-          <b-dropdown-text v-if="user.isPlayer">
-            Участник&nbsp;эвента
-          </b-dropdown-text>
-          <b-dropdown-divider v-if="user.isAdmin || user.isPlayer"/>
-          <b-dropdown-item-button @click="logout">
-            Выход
-          </b-dropdown-item-button>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+      <b-navbar-brand to="/">
+        Asuka
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"/>
+      <b-collapse
+        id="nav-collapse"
+        is-nav
+      >
+        <MainMenu/>
+        <b-navbar-nav class="ml-auto">
+          <Login
+            v-if="!user.id"
+            class="mr-1"
+          />
+          <Restore
+            v-if="!user.id"
+            class="mr-1"
+          />
+          <SignUp v-if="!user.id"/>
+          <b-nav-item-dropdown
+            v-else
+            right
+          >
+            <template slot="button-content">
+              {{ user.name }}
+            </template>
+            <b-dropdown-text v-if="user.isAdmin">
+              Администратор
+            </b-dropdown-text>
+            <b-dropdown-text v-if="user.isPlayer">
+              Участник&nbsp;эвента
+            </b-dropdown-text>
+            <b-dropdown-divider v-if="user.isAdmin || user.isPlayer"/>
+            <b-dropdown-item-button @click="logout">
+              Выход
+            </b-dropdown-item-button>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <Message/>
+    <Modal/>
+  </div>
 </template>
 
 <script>
-import {mapActions, mapState} from 'vuex'
-import Login from './Login'
-import Restore from './Restore'
-import SignUp from './SignUp'
-import MainMenu from '../MainMenu'
+  import {mapActions, mapState} from 'vuex'
+  import Login from './Login'
+  import Restore from './Restore'
+  import SignUp from './SignUp'
+  import MainMenu from '../MainMenu'
+  import Message from '../../Message'
+  import Modal from '../../Modal'
 
-export default {
+  export default {
   components: {
     Login,
     Restore,
     SignUp,
-    MainMenu
+    MainMenu,
+    Message,
+    Modal
   },
   computed: {
     ...mapState({
