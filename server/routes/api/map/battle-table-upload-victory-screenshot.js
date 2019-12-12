276185ea7e53acb9fbf498586891736fe6959297
tskon <tsekhmeister.k@gmail.com>
2019-12-12 22:57:26 +0300

    add todos and first data handling

--- server/routes/api/map/battle-table-upload-victory-screenshot.js
+++ server/routes/api/map/battle-table-upload-victory-screenshot.js
@@ -1,20 +1,33 @@
 module.exports = function (router, models) {
   router.post('/map/battle-table-upload-victory-screenshot', (req, res) => {
-    console.log(req.files, req.file)
-    // models.battleTable.findOne({
-    //   where: {
-    //     turnNumber: req.body.turnNumber,
-    //     cellId: req.body.cellId
-    //   }
-    // })
-    //   .then((battleTable) => {
-    //     if (!battleTable) {
-    //       return
-    //     }
-    //
-    //     res.send({
-    //       status: 'ok'
-    //     })
-    //   })
+    console.log(req.files)
+    models.battleTable.findOne({
+      where: {
+        // turnNumber: req.body.turnNumber, // заранее получать turnNumber
+        cellId: req.body.cellId
+      }
+    })
+      .then((battleTable) => {
+        if (!battleTable) {
+          const screenshots = {}
+          if (req.body.isFinal) {
+            screenshots.final[req.body.clanTag] = req.files.screenshot
+          } else {
+            screenshots.semifinals[req.body.clanTag] = req.files.screenshot
+          }
+
+          models.battleTable.create({
+            // TODO запрашивать turnNumber: NUM,
+            cellId: req.body.cellId,
+            screenshots: JSON.stringify(screenshots)
+          })
+        } else {
+          // TODO получать уже имеющиеся данные, расширять пришедшими
+        }
+
+        res.send({
+          status: 'ok'
+        })
+      })
   })
 }
