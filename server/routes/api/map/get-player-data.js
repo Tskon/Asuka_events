511957c74ab162c1f0812c6e96cb1a28677c76aa
Tskon <tsekhmeister.k@gmail.com>
2020-01-03 21:39:55 +0300

    refactoring

--- server/routes/api/map/get-player-data.js
+++ server/routes/api/map/get-player-data.js
@@ -28,28 +28,6 @@ module.exports = function (router, models) {
           ? currentCell.connectedCells
           : cellList.filter((cell) => cell.isStarted)
 
-        let inBattle = false
-        if (currentCell && currentCell.players.length > 1) {
-
-          rawBattleTableList.some(rawBattleTable => {
-            const battleTable = JSON.parse(rawBattleTable)
-            if (battleTable.pair1.includes(req.user.id) && battleTable.pair1.length > 1) {
-              if (!battleTable.winner) inBattle = true
-              return true
-            }
-            if (battleTable.pair2.includes(req.user.id) && battleTable.pair2.length > 1) {
-              if (!battleTable.winner) inBattle = true
-              return true
-            }
-            if (battleTable.finalPair.includes(req.user.id) && battleTable.finalPair.length > 1) {
-              if (!battleTable.winner) inBattle = true
-              return true
-            }
-
-            return false
-          })
-        }
-
         res.send({
           status: 'ok',
           data: {
@@ -57,9 +35,34 @@ module.exports = function (router, models) {
             selectedCellId: userMapData.selectedCellId,
             selectableCellIds,
             score: userMapData.score,
-            inBattle
+            inBattle: checkInBattleStatus()
           }
         })
+
+        function checkInBattleStatus() {
+          let inBattle = false
+          if (currentCell && currentCell.players.length > 1) {
+            rawBattleTableList.some(rawBattleTable => {
+              const battleTable = JSON.parse(rawBattleTable)
+              if (battleTable.pair1.includes(req.user.id) && battleTable.pair1.length > 1) {
+                if (!battleTable.winner) inBattle = true
+                return true
+              }
+              if (battleTable.pair2.includes(req.user.id) && battleTable.pair2.length > 1) {
+                if (!battleTable.winner) inBattle = true
+                return true
+              }
+              if (battleTable.finalPair.includes(req.user.id) && battleTable.finalPair.length > 1) {
+                if (!battleTable.winner) inBattle = true
+                return true
+              }
+
+              return false
+            })
+          }
+
+          return inBattle
+        }
       })
   })
 }
