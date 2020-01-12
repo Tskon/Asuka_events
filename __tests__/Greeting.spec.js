bc22bfc76f1881d47dd0a8c020251d247fa87ee2
Tskon <tsekhmeister.k@gmail.com>
2020-01-12 15:58:14 +0300

    del fake test

--- __tests__/Greeting.spec.js
+++
@@ -1,9 +0,0 @@
-import {mount} from '@vue/test-utils'
-import Greeting from "../src/components/Greeting"
-
-describe('Greeting.vue', () => {
-  it('отрисовка приветствия', () => {
-    const wrapper = mount(Greeting)
-    expect(wrapper.text()).toMatch('Vue и TDD')
-  })
-})
