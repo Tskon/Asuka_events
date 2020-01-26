f90ea8d215b54ebc7da3b1289f8af8e1278048a6
Tskon <tsekhmeister.k@gmail.com>
2020-01-26 14:38:11 +0300

    payload handler

--- server/routes/api/map/get-battle-table-data.js
+++ server/routes/api/map/get-battle-table-data.js
@@ -1,31 +1,19 @@
 module.exports = function (router, models) {
   router.post('/map/get-battle-table-data', async (req, res) => {
-    const [mapLog, mapCellsRaw] = await Promise.all([
-      models.mapLog.findAll({
-        limit: 1,
-        order: [['turn', 'DESC']],
-        attributes: ['turn']
-      }),
-      models.mapCell.findAll()
-    ])
-
-    const mapCells = mapCellsRaw.map(cell => {
-      return {
-        cellName: cell.cellName,
-        ...JSON.parse(cell.dataJson)
-      }
+    const mapLog = await models.mapLog.findAll({
+      limit: 1,
+      order: [['turn', 'DESC']],
+      attributes: ['turn']
     })
-    const currentCell = mapCells.find(cell => cell.players.includes(req.user.id))
+
     const turnNumber = (mapLog.length) ? mapLog[0].turn + 1 : 1
 
-    const battleTable = currentCell 
-      ? await models.battleTable.findOne({
-        where: {
-          turnNumber,
-          cellId: currentCell.cellName
-        }
-      })
-      : null
+    const battleTable = await models.battleTable.findOne({
+      where: {
+        turnNumber,
+        cellId: req.body.cellId
+      }
+    })
 
     if (battleTable) {
       res.send({
