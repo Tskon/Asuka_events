c2de8e525dbcb5f3d1b5e9ad0a12bf49124b9b11
Tskon <tsekhmeister.k@gmail.com>
2020-04-07 20:18:33 +0300

    add test data reset

--- server/routes/api/admin/clean-event-data.js
+++ server/routes/api/admin/clean-event-data.js
@@ -1,76 +1,27 @@
-const Sequelize = require('sequelize')
-
+//TODO наладить корректный сброс данных
 module.exports = function (router, models) {
   router.post('/admin/clean-event-data', async (req, res) => {
-    models.mapCell.drop()
-    models.battleTable.drop()
-    models.mapLog.drop()
-    models.mapTurnData.drop()
-    models.userMapData.drop()
-
-    models.mapCell.create({
-      id: {
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-
-      cellName: {
-        type: Sequelize.CHAR(4),
-        defaultValue: 'a1'
-      },
-
-      dataJson: {
-        type: Sequelize.TEXT,
-        defaultValue: '{"connectedCells":[], "isStarted": false, "bonus":0, "controlledTurnsCount": 0, "players": [], "owner": null}'
-      }
+    models.mapCell.destroy({
+      where: {},
+      truncate: true
     })
-    models.battleTable.create({
-      id: {
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-
-      turnNumber: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0
-      },
-
-      cellId: {
-        type: Sequelize.CHAR(4),
-        defaultValue: ''
-      },
-
-      dataJson: {
-        type: Sequelize.TEXT,
-        defaultValue: `{
-      "screenshots": {
-        "finalist1": "",
-        "finalist2": "",
-        "winner": ""
-      },
-      "pair1": [],
-      "pair2": [],
-      "finalPair": [],
-      "winner": ""
-    }`
-      }
+    models.battleTable.destroy({
+      where: {},
+      truncate: true
     })
-    models.mapLog.drop({
-      turn: {
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      playersJson: {
-        type: Sequelize.TEXT,
-        defaultValue: ''
-      },
-      cellsJson: {
-        type: Sequelize.TEXT,
-        defaultValue: ''
-      }
+    models.mapLog.destroy({
+      where: {},
+      truncate: true
+    })
+
+    models.userMapData.destroy({
+      where: {},
+      truncate: true
+    })
+
+    res.send({
+      status: 'info',
+      message: 'Данные эвента успешно удалены'
     })
   })
 }
