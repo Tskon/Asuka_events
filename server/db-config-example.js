9378a1d1853bd55568a15818e6812551dd03e079
Tskon <tsekhmeister.k@gmail.com>
2020-04-15 10:14:38 +0300

    add new position to git-ignore

---
+++ server/db-config-example.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dbName: 'dbName',
+  dbUser: 'user',
+  dbPassword: 'password',
+  dbOptions: {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    logging: false
+  }
+}
