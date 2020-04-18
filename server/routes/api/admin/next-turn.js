91ff301aa4e2ac2f10624f309073b0d928a1b14e
Tskon <tsekhmeister.k@gmail.com>
2020-04-18 19:43:08 +0300

    del waste todo

--- server/routes/api/admin/next-turn.js
+++ server/routes/api/admin/next-turn.js
@@ -1,5 +1,3 @@
-// TODO добавлять очки оунерам клеток
-
 module.exports = function (router, models) {
   router.post('/admin/next-turn', async (req, res) => {
     const [mapLog] = await models.mapLog.findAll({
