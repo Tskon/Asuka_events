4cf74140b41901bdf311c7d47be32ae47f345d7e
Tskon <tsekhmeister.k@gmail.com>
2020-04-18 21:11:28 +0300

    change start users data

--- server/dbInitData/users.js
+++ server/dbInitData/users.js
@@ -7,105 +7,80 @@ module.exports = (models) => {
           password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
           secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
           isAdmin: 1,
-          isPlayer: 1
+          isPlayer: 0
         })
-
-        models.user.create({
-          username: 'user1',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'user2',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'user3',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'user4',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'user5',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'user6',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'user7',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'user8',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'user9',
-          password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
-          isAdmin: 0,
-          isPlayer: 1
-        })
-
-        models.user.create({
-          username: 'Palochka_Nagibalochka_M',
-          password: '$2a$08$qbgtfzddmKniXHGrnbZEVeMwopRcbnybTtVfZrUVEd5pBDS4aigry',
-          secret: '$2a$08$qbgtfzddmKniXHGrnbZEVeMwopRcbnybTtVfZrUVEd5pBDS4aigry',
-          isAdmin: 1,
-          isPlayer: 1
-        })
-
-        models.userMapData.create({
-          userId: 1,
-          selectedCellId: 'a4'
-        })
-        models.userMapData.create({
-          userId: 2,
-          selectedCellId: 'a4'
-        })
-        models.userMapData.create({
-          userId: 3,
-          selectedCellId: 'a4'
-        })
-        models.userMapData.create({
-          userId: 4,
-          selectedCellId: 'a4'
-        })
+        //
+        // models.user.create({
+        //   username: 'user1',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
+        //
+        // models.user.create({
+        //   username: 'user2',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
+        //
+        // models.user.create({
+        //   username: 'user3',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
+        //
+        // models.user.create({
+        //   username: 'user4',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
+        //
+        // models.user.create({
+        //   username: 'user5',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
+        //
+        // models.user.create({
+        //   username: 'user6',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
+        //
+        // models.user.create({
+        //   username: 'user7',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
+        //
+        // models.user.create({
+        //   username: 'user8',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
+        //
+        // models.user.create({
+        //   username: 'user9',
+        //   password: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   secret: '$2a$08$Dygxt1hYXnEcEvaWOQqSv.m/U4XHbfeGbE1Oo17SsTQM99wdRWvTi',
+        //   isAdmin: 0,
+        //   isPlayer: 1
+        // })
       }
     })
 }
