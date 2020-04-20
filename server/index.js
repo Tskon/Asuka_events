02490331582a71f75f055a0224b35c5dade7838d
Tskon <tsekhmeister.k@gmail.com>
2020-04-21 00:13:11 +0300

    create connection + test file for mongo

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const passport = require('passport')
 const fileUpload = require('express-fileupload')
@@ -7,82 +8,90 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
+const mongoose = require('mongoose')
 const models = require('./models/index')
 const dbConfig = require('./db-config')
 
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+
+
 const PORT = process.env.PORT || 12345
-
-const options = {
-  host: dbConfig.dbOptions.host,
-  port: dbConfig.dbOptions.port,
-  user: dbConfig.dbUser,
-  password: dbConfig.dbPassword,
-  database: dbConfig.dbName
-}
-
+//
+// const options = {
+//   host: dbConfig.dbOptions.host,
+//   port: dbConfig.dbOptions.port,
+//   user: dbConfig.dbUser,
+//   password: dbConfig.dbPassword,
+//   database: dbConfig.dbName
+// }
+//
 const app = express()
-app.use(cors())
-  .use(fileUpload({ createParentPath: true }))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(cookieParser())
-  .use(session({
-    store: new MySQLStore(options),
-    secret: 'asuka and ray',
-    resave: true,
-    saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } // 7 суток живут сессии
-  }))
-  .use(passport.initialize())
-  .use(passport.session())
-
-  // Serve the static files from the React app
-  .use(express.static(path.resolve(__dirname, '../dist')))
-
-  /**
-   * Routes. use path /api + path in routes.
-   * For example: /api/auth
-   */
-  .use('/api/user', (req, res, next) => {
-    if (req.isAuthenticated()) next()
-    else res.send({ status: 'error', message: 'Вы не авторизованы. User' })
-  })
-  .use('/api/admin', (req, res, next) => {
-    if (req.user && req.user.isAdmin) next()
-    else res.send({ status: 'error', message: 'Не достаточно прав. Admin' })
-  })
-  .use('/api/map', (req, res, next) => {
-    if (req.user && (req.user.isAdmin || req.user.isPlayer)) next()
-    else res.send({ status: 'error', message: 'Не достаточно прав. Player' })
-  })
-  .use('/api', require('./routes/api/index')(passport, app))
-
-  /**
-   * for all the react stuff
-   */
-  .get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../dist/index.html'))
-  })
-
-/**
- * Load passport strategies
- *
- */
-require('./passport/passport.js')(passport, models.user)
-
-/**
- * Sync with database
- */
-models.sequelize.sync()
-  .then(() => {
-    require('./dbInitData/mapCells')(models)
-    require('./dbInitData/users')(models)
-    require('./dbInitData/mapTurnsData')(models)
-    console.log('Nice! Database looks fine')
-  })
-  .catch((err) => {
-    console.log(err, 'Something went wrong with the Database Update!')
-  })
+// app.use(cors())
+//   .use(fileUpload({ createParentPath: true }))
+//   .use(bodyParser.json())
+//   .use(bodyParser.urlencoded({ extended: true }))
+//   .use(cookieParser())
+//   .use(session({
+//     store: new MySQLStore(options),
+//     secret: 'asuka and ray',
+//     resave: true,
+//     saveUninitialized: false,
+//     cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } // 7 суток живут сессии
+//   }))
+//   .use(passport.initialize())
+//   .use(passport.session())
+//
+//   // Serve the static files from the React app
+//   .use(express.static(path.resolve(__dirname, '../dist')))
+//
+//   /**
+//    * Routes. use path /api + path in routes.
+//    * For example: /api/auth
+//    */
+//   .use('/api/user', (req, res, next) => {
+//     if (req.isAuthenticated()) next()
+//     else res.send({ status: 'error', message: 'Вы не авторизованы. User' })
+//   })
+//   .use('/api/admin', (req, res, next) => {
+//     if (req.user && req.user.isAdmin) next()
+//     else res.send({ status: 'error', message: 'Не достаточно прав. Admin' })
+//   })
+//   .use('/api/map', (req, res, next) => {
+//     if (req.user && (req.user.isAdmin || req.user.isPlayer)) next()
+//     else res.send({ status: 'error', message: 'Не достаточно прав. Player' })
+//   })
+//   .use('/api', require('./routes/api/index')(passport, app))
+//
+//   /**
+//    * for all the react stuff
+//    */
+//   .get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+//   })
+//
+// /**
+//  * Load passport strategies
+//  *
+//  */
+// require('./passport/passport.js')(passport, models.user)
+//
+// /**
+//  * Sync with database
+//  */
+// models.sequelize.sync()
+//   .then(() => {
+//     require('./dbInitData/mapCells')(models)
+//     require('./dbInitData/users')(models)
+//     require('./dbInitData/mapTurnsData')(models)
+//     console.log('Nice! Database looks fine')
+//   })
+//   .catch((err) => {
+//     console.log(err, 'Something went wrong with the Database Update!')
+//   })
 
 /**
  * Listening port by express
