3f6e93eca3e23aa47e78bb9675e6ce52851b832b
Tskon <tsekhmeister.k@gmail.com>
2020-04-24 09:52:15 +0300

    add models to index

--- server/dbModels/index.js
+++ server/dbModels/index.js
@@ -1,7 +1,11 @@
 const User = require('./User')
 const Player = require('./Player')
+const Cell = require('./Cell')
+const TurnType = require('./TurnType')
 
 module.exports = {
   User,
-  Player
+  Player,
+  Cell,
+  TurnType
 }
