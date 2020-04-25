ba397dff5b955b2b8b5609a353eef7bd8d352ce3
Tskon <tsekhmeister.k@gmail.com>
2020-04-25 17:05:24 +0300

    add init data for turns

--- server/dbInitData/mapTurnsData.js
+++ server/dbInitData/mapTurnsData.js
@@ -1,7 +1,7 @@
 module.exports = async (models) => {
   const turns = [
     { type: 'selectStartSector', count: 1, fog: false },
-    { type: 'commonTurn', count: 9, fog: true }
+    { type: 'commonTurn', count: 99, fog: true }
   ]
 
   if (await models.TurnType.countDocuments()) return
