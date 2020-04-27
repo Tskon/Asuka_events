472d8f4371a4c59e8190d99fe1b038e65007397d
Tskon <tsekhmeister.k@gmail.com>
2020-04-28 00:46:51 +0300

    fix set-player-status

--- server/routes/api/admin/set-player-status.js
+++ server/routes/api/admin/set-player-status.js
@@ -2,6 +2,10 @@ module.exports = function (router, models) {
   router.post('/admin/set-player-status', async (req, res) => {
     await models.User.updateOne({ username: req.body.username }, { isPlayer: req.body.status })
 
+    if (!await models.Player.findOne({ username: req.body.username })) {
+      await models.Player.create({ username: req.body.username })
+    }
+
     res.send({
       status: 'ok'
     })
