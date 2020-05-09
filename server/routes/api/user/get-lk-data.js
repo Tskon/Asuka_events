15f6bff8cffe2d4b1d64de4b057d449ceba48024
Tskon <tsekhmeister.k@gmail.com>
2020-05-09 23:56:59 +0300

    del waste validations

--- server/routes/api/user/get-lk-data.js
+++ server/routes/api/user/get-lk-data.js
@@ -1,13 +1,5 @@
 module.exports = function (router, models) {
   router.post('/user/get-lk-data', (req, res) => {
-    if (!req.user || !req.user.isPlayer) {
-      return res.send({
-        status: 'error',
-        message: 'Нет прав игрока',
-        goTo: '/'
-      })
-    }
-
     models.User.findOne({ username: req.user.username }, (err, user) => {
       if (user) {
         res.send({
