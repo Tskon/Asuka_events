5f4e1969c6709571d82757c906c1f70bb04bf61a
Tskon <tsekhmeister.k@gmail.com>
2020-05-19 19:46:35 +0300

    add event model

---
+++ server/dbModels/Event.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const EventSchema = new Schema({
+  eventSlug: String,
+  eventName: String,
+  columns: Array,
+  rows: Array,
+  startedSectors: Array,
+  richEconomyCells: {
+    list: Array,
+    bonus: Number
+  },
+  middleEconomyCells: {
+    list: Array,
+    bonus: Number
+  },
+  poorEconomyCells: {
+    list: Array,
+    bonus: Number
+  },
+  gameMapList: Array
+})
+
+module.exports = mongoose.model('Event', EventSchema)
