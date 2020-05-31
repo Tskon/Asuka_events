cf697ee3f25666469c0af9bfc66b0b81e8f30456
Tskon <tsekhmeister.k@gmail.com>
2020-05-31 18:21:11 +0300

    change event get api

--- server/dbModels/Player.js
+++ server/dbModels/Player.js
@@ -4,11 +4,16 @@ const {Schema} = mongoose
 
 const PlayerSchema = new Schema({
   username: String,
-  score: { type: Number, default: 0 },
-  currentCell: { type: String, default: '' },
-  selectedCell: { type: String, default: '' },
-  ownedCell: { type: String, default: '' },
-  ownInRowCount: { type: Number, default: 0 }
+  events: [
+    {
+      slug: String,
+      score: { type: Number, default: 0 },
+      currentCell: { type: String, default: '' },
+      selectedCell: { type: String, default: '' },
+      ownedCell: { type: String, default: '' },
+      ownInRowCount: { type: Number, default: 0 }
+    }
+  ]
 })
 
 module.exports = mongoose.model('Player', PlayerSchema)
--- server/routes/api/event/get-event-list.js
+++ server/routes/api/event/get-event-list.js
@@ -1,11 +1,85 @@
 module.exports = function (router, models) {
   router.post('/event/get-event-list', async (req, res) => {
-    const eventList = await models.Event.find()
+    const [eventList, players, users] = await Promise.all([
+      models.Event.find(),
+      models.Player.find({}, 'username events'),
+      models.User.find({ isPlayer: true }, 'username clanTag clanName avatar'),
+    ])
+
+    const parsedEventList = eventList.map(async event => {
+      const eventPlayers = []
+
+      players.forEach(player => {
+        const findedPlayer = player.events.find(playerEvent => playerEvent.slug === event.slug)
+        const findedUser = users.find(user => user.username === findedPlayer.username)
+        const findedPlayerEvent = findedPlayer.events.find(playerEvent => playerEvent.slug === event.slug)
+
+        const changedPlayer = {
+          ...findedUser,
+          score: findedPlayerEvent.score,
+          currentCell: findedPlayerEvent.currentCell,
+          selectedCell: findedPlayerEvent.selectedCell,
+          ownedCell: findedPlayerEvent.ownedCell,
+          ownInRowCount: findedPlayerEvent.ownInRowCount
+        }
+
+        eventPlayers.push(changedPlayer)
+      })
+
+      const turnsCount = await models.Log.countDocuments({eventSlug: event.slug})
+
+      const currentTurn = await models.TurnType.findOne({
+        turnNumber: turnsCount + 1
+      }, 'number fog type')
+
+      const cellsWithPlayers = event.cellList.map((cell) => {
+        const { name, started, bonus, connectedCells, gameMap } = cell
+        const filteredPlayers = eventPlayers.filter(player => player.currentCell === cell.name)
+
+        return {
+          name,
+          gameMap,
+          started,
+          bonus,
+          connectedCells,
+          players: filteredPlayers.map(player => {
+            return users.find(user => user.username === player.username)
+          })
+        }
+      })
+
+      const currentCell = cellsWithPlayers.find((cell) => {
+        return cell.players.some(player => player.username === req.user.username)
+      })
+
+      const filteredData = cellsWithPlayers.map((cell) => {
+        const isFullData = currentCell
+          ? cell.name === currentCell.name || cell.connectedCells.includes(currentCell.name)
+          : cell.started || req.user.isAdmin
+
+        return isFullData ? cell : {
+          name: cell.name,
+          connectedCells: cell.connectedCells,
+          players: [],
+          bonus: cell.bonus,
+          started: cell.started,
+          gameMap: cell.gameMap
+        }
+      })
+
+      return {
+        slug: event.slug,
+        name: event.name,
+        bonusForWin: event.bonusForWin,
+        turnList: event.turnList,
+        cellList: (currentTurn && currentTurn.fog) ? filteredData : cellsWithPlayers
+      }
+    })
 
     res.send({
       status: 'ok',
       data: {
-        eventList
+        eventList: parsedEventList
       }
     })
   })
