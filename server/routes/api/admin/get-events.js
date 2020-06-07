489b8c76e26e3d3de676cf1304932022699c204f
Tskon <tsekhmeister.k@gmail.com>
2020-06-07 18:39:12 +0300

    add turnNumber for get-events api

--- server/routes/api/admin/get-events.js
+++ server/routes/api/admin/get-events.js
@@ -1,11 +1,22 @@
 module.exports = function (router, models) {
   router.post('/admin/get-events', async (req, res) => {
-    const events = await models.Event.find({}, 'name slug')
+    const [events, logs] = await Promise.all([
+      models.Event.find({}, 'name slug'),
+      models.Log.find()
+    ])
+
+    const eventsWithTurnNumbers = events.map(event => {
+      const filteredLog = logs.filter(log => log.eventSlug === event.slug)
+      return {
+        ...event._doc,
+        turnNumber: filteredLog.length + 1
+      }
+    })
 
     res.send({
       status: 'success',
       data: {
-        events
+        events: eventsWithTurnNumbers
       }
     })
   })
