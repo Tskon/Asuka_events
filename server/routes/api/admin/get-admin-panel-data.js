b57ea082b4964e820e13846fc0ef2c64ec427f38
Tskon <tsekhmeister.k@gmail.com>
2020-06-12 16:54:12 +0300

    fix get-admin-panel-data api

--- server/routes/api/admin/get-admin-panel-data.js
+++ server/routes/api/admin/get-admin-panel-data.js
@@ -3,37 +3,24 @@ module.exports = function (router, models) {
     const users = await models.User.find()
     const playersData = await models.Player.find()
 
-    const commonUsers = []
-    const players = []
-    const admins = []
-
-    users.forEach((user) => {
+    const usersData = users.map((user) => {
       const {
-        username, isAdmin, isPlayer
+        username, isAdmin
       } = user
-
-      const userObject = {
-        username,
-        isAdmin,
-        isPlayer
-      }
 
       const playerData = playersData.find(pd => pd.username === username) || {_doc: {}}
 
-      if (isAdmin) admins.push(userObject)
-      if (isPlayer) players.push({
-        ...userObject,
-        ...playerData._doc
-      })
-      if (!isPlayer) commonUsers.push(userObject)
+      return {
+        username,
+        isAdmin,
+        eventList: playerData.events
+      }
     })
 
     res.send({
       status: 'ok',
       data: {
-        commonUsers,
-        players,
-        admins
+        users: usersData
       }
     })
 
