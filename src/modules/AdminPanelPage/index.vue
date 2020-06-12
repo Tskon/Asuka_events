ff6bb38a3d1f8e76b9c3cd25eb310244ae133b1d
Tskon <tsekhmeister.k@gmail.com>
2020-06-12 15:08:28 +0300

    new admin-panel

--- src/modules/AdminPanelPage/index.vue
+++ src/modules/AdminPanelPage/index.vue
@@ -1,78 +1,92 @@
 <template>
   <div class="admin-panel pb-3">
-    <PlayersTable class="players-table"/>
-    <GeneralState
-      v-for="event in eventList"
-      :key="event.slug"
-      :current-event="event"
-      class="event-state"
-    />
-    <div class="card card-body">
-      <h3>Пользователи:</h3>
-      <b-card
-        no-body
-        header="Администраторы"
-        class="admins-list"
-      >
-        <b-list-group flush>
-          <b-list-group-item
-            v-for="admin in adminData.users.admins"
-            :key="admin.username"
-          >
-            {{ admin.username }}
-          </b-list-group-item>
-        </b-list-group>
-      </b-card>
+    <b-card
+      class="events"
+      no-body
+    >
+      <b-tabs card>
+        <b-tab
+          v-for="event in eventList"
+          :key="event.slug"
+          :title="event.name"
+          active
+        >
+          <div class="event-tab">
+            <GeneralState
+              :current-event="event"
+              class="event-tab__state"
+            />
+            <div class="event-tab__user-table card card-body">
+              <h3>Пользователи:</h3>
+              <b-card
+                no-body
+                header="Администраторы"
+                class="admins-list"
+              >
+                <b-list-group flush>
+                  <b-list-group-item
+                    v-for="admin in adminData.users.admins"
+                    :key="admin.username"
+                  >
+                    {{ admin.username }}
+                  </b-list-group-item>
+                </b-list-group>
+              </b-card>
 
-      <b-card
-        no-body
-        header="Игроки"
-        class="users-list"
-      >
-        <b-list-group flush>
-          <b-list-group-item
-            v-for="player in adminData.users.players"
-            :key="player.username"
-          >
-            {{ player.username }}
-            <b-button
-              variant="danger"
-              @click="setPlayerStatus({
-                username: player.username,
-                status: false
-              })"
-            >
-              Удалить права
-            </b-button>
-          </b-list-group-item>
-        </b-list-group>
-      </b-card>
+              <b-card
+                no-body
+                header="Игроки"
+                class="users-list"
+              >
+                <b-list-group flush>
+                  <b-list-group-item
+                    v-for="player in adminData.users.players"
+                    :key="player.username"
+                  >
+                    {{ player.username }}
+                    <b-button
+                      variant="danger"
+                      @click="setPlayerStatus({
+                        username: player.username,
+                        status: false
+                      })"
+                    >
+                      Удалить права
+                    </b-button>
+                  </b-list-group-item>
+                </b-list-group>
+              </b-card>
 
-      <b-card
-        no-body
-        header="Обычные пользователи"
-        class="users-list"
-      >
-        <b-list-group flush>
-          <b-list-group-item
-            v-for="user in adminData.users.commonUsers"
-            :key="user.username"
-          >
-            {{ user.username }}
-            <b-button
-              variant="success"
-              @click="setPlayerStatus({
-                username: user.username,
-                status: true
-              })"
-            >
-              Сделать игроком
-            </b-button>
-          </b-list-group-item>
-        </b-list-group>
-      </b-card>
-    </div>
-    <EventsCRUD/>
+              <b-card
+                no-body
+                header="Обычные пользователи"
+                class="users-list"
+              >
+                <b-list-group flush>
+                  <b-list-group-item
+                    v-for="user in adminData.users.commonUsers"
+                    :key="user.username"
+                  >
+                    {{ user.username }}
+                    <b-button
+                      variant="success"
+                      @click="setPlayerStatus({
+                        username: user.username,
+                        status: true
+                      })"
+                    >
+                      Сделать игроком
+                    </b-button>
+                  </b-list-group-item>
+                </b-list-group>
+              </b-card>
+            </div>
+            <PlayersTable class="event-tab__players-table"/>
+          </div>
+        </b-tab>
+      </b-tabs>
+    </b-card>
+    <CreateEvent class="new-event card card-body"/>
   </div>
 </template>
 
@@ -80,7 +94,7 @@
 import { mapActions, mapState, mapGetters } from 'vuex'
 import GeneralState from './components/GeneralState'
 import PlayersTable from './components/PlayersTable'
-import EventsCRUD from './components/EventsCRUD'
+import CreateEvent from './components/CreateEvent'
 
 export default {
   name: 'AdminPanelPage',
@@ -88,7 +102,7 @@ export default {
   components: {
     GeneralState,
     PlayersTable,
-    EventsCRUD
+    CreateEvent
   },
 
   data() {
@@ -125,73 +139,35 @@ export default {
 
 <style scoped lang="scss">
 .admin-panel {
-  padding-top: 20px;
+  margin-left: -15px;
+  margin-right: -15px;
+  padding: 20px;
   display: grid;
   grid-template-rows: auto;
   grid-template-columns: 1fr;
   grid-gap: 20px;
+  background-color: lighten($dark, 20%);
 
   @media (min-width: $md) {
     grid-template-columns: 1fr 1fr;
+
+    .events {
+      grid-column: 1 / span 2;
+    }
   }
-  @media (min-width: $lg) {
+
+  @media (min-width: $xl) {
     grid-template-columns: 1fr 1fr 1fr;
   }
 }
 
-.event-state {
-  @media (min-width: $lg) {
-    grid-column: 3;
-  }
-}
-
-.logs {
-  grid-row: 2;
-  grid-column: 1 / span 3;
-}
-
-.admins-list {
-  margin-bottom: 20px;
-
-  .list-group-item {
-    padding: 7px 0 7px 1.25rem;
-  }
-}
-
-.users-list {
-  margin-bottom: 20px;
-
-  .list-group-item {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 0 0 1.25rem;
-
-    .btn {
-      border-top-right-radius: 0;
-    }
-
-    &:not(:last-child) .btn {
-      border-bottom-right-radius: 0;
-    }
-  }
-}
-
-.cells-grid {
+.event-tab {
   display: grid;
-  grid-template-rows: repeat(6, 1fr);
-  grid-template-columns: repeat(8, 1fr);
-  grid-auto-flow: column;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
 
-  .cells-grid-item {
-    width: 160px;
-  }
-}
-
-.players-table {
-  @media (min-width: $md) {
-    grid-column: span 2;
-    grid-row: span 2;
+  &__players-table {
+    grid-column: 1 / span 2;
   }
 }
 </style>
