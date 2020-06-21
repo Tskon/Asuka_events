ab02cba50d830d149308aee59563647339d00d20
Tskon <tsekhmeister.k@gmail.com>
2020-06-21 16:50:34 +0300

    repair player data. Add battle status and selectable cells

--- server/routes/api/event/get-player-data.js
+++ server/routes/api/event/get-player-data.js
@@ -6,6 +6,10 @@ module.exports = function (router, models) {
       return models.Log.find({ eventSlug: event.slug })
     }))
 
+    const eventsData = await Promise.all(playerData.events.map(event => {
+      return models.Event.findOne({ slug: event.slug })
+    }))
+
     const battleTables = await Promise.all(playerData.events.map((event, i) => {
       return models.BattleTable.find({
         eventSlug: event.slug,
@@ -20,15 +24,23 @@ module.exports = function (router, models) {
 
     const playerDataByEvents = playerData.events.map((event, i) => {
       const turnNumber = logListsByEvent[i].filter(log => log.eventSlug === event.slug).length + 1
+
       const battleTable = battleTables.find(bt => {
         return bt.eventSlug === event.slug && bt.turnNumber === turnNumber
       })
+      const battleStatus = checkBattleStatus(battleTable, req.username)
+
+      const currentCellData = eventsData[i].cellList.find(cell => cell.name === event.currentCell)
+      const filter = (battleStatus.winner && currentCellData)
+        ? cell => cell.name === currentCellData.name || currentCellData.connectedCells.includes(cell.name)
+        : cell => cell.started
+      const selectableCells = eventsData[i].cellList.filter(filter).map(cell => cell.name)
 
       const extendedEvent = {
         ...event._doc,
         _id: null,
-        selectableCells: [],
-        battleStatus: checkBattleStatus(battleTable, req.username)
+        selectableCells,
+        battleStatus
       }
 
       return extendedEvent
@@ -66,38 +78,3 @@ function checkBattleStatus(battleTable, username) {
     winner
   }
 }
-
-
-// module.exports = function (router, models) {
-//   router.post('/map/get-player-data', async(req, res) => {
-//     let playerData = await models.Player.findOne({username: req.user.username})
-//
-//     if (!playerData) {
-//       await models.Player.create({ username: req.user.username })
-//       playerData = await models.Player.findOne({username: req.user.username})
-//     }
-//
-//     const turnsCount = await models.Log.countDocuments()
-//
-//     const [battleTable, cells] = await Promise.all([
-//       models.BattleTable.findOne({ turnNumber: turnsCount, cellName: playerData.currentCell }),
-//       models.Cell.find()
-//     ])
-//
-//     const currentCell = cells.find(cell => cell.name === playerData.currentCell)
-//     const battleStatus = checkBattleStatus(battleTable, req.user.username)
-//     const filter = (battleStatus.winner && currentCell)
-//       ? cell => cell.name === currentCell.name || currentCell.connectedCells.includes(cell.name)
-//       : cell => cell.started
-//     const selectableCells = cells.filter(filter).map(cell => cell.name)
-//
-//     res.send({
-//       status: 'ok',
-//       data: {
-//         selectableCells,
-//         battleStatus
-//       }
-//     })
-
-//   })
-// }
