5c7dd956a48c372b1f7789d17e8b8f35aec466db
Tskon <tsekhmeister.k@gmail.com>
2020-08-08 16:51:13 +0300

    change previous battle table

--- server/routes/api/admin/set-player-data.js
+++ server/routes/api/admin/set-player-data.js
@@ -26,8 +26,12 @@ module.exports = function (router, models) {
       return
     }
 
+    let prevCurrentCell
+
     playerFromDB.events.some(event => {
       if (event.slug === eventSlug) {
+        prevCurrentCell = event.currentCell
+
         event.score = score
         event.currentCell = currentCell
         event.selectedCell = selectedCell
@@ -42,12 +46,22 @@ module.exports = function (router, models) {
     await models.Player.updateOne({ username } , playerFromDB)
 
     const players = await models.Player.find({ events : { $elemMatch: {  slug : { $gte: eventSlug } } } })
+
+    const playerListInPrevCurrentCell = players.filter(player => {
+      return player.events.some(event => event.slug === eventSlug && event.currentCell === prevCurrentCell)
+    })
     const playerListInCurrentCell = players.filter(player => {
       return player.events.some(event => event.slug === eventSlug && event.currentCell === currentCell)
     })
 
+    if (playerListInPrevCurrentCell.length > 1) {
+      createBattleTable(playerListInCurrentCell.map(player => player.username), users, prevCurrentCell)
+    } else {
+      await models.BattleTable.deleteOne({ eventSlug, turnNumber, cellName: prevCurrentCell })
+    }
+
     if (playerListInCurrentCell.length > 1) {
-      createBattleTable(playerListInCurrentCell.map(player => player.username), users)
+      createBattleTable(playerListInCurrentCell.map(player => player.username), users, currentCell)
     }
 
     res.send({
@@ -55,7 +69,7 @@ module.exports = function (router, models) {
       message: 'Данные успешно перезаписаны'
     })
 
-    function createBattleTable(playerList, users) {
+    async function createBattleTable(playerList, users, cell) {
       const firstPair = { winner: null, looser: null }
       const secondPair = { winner: null, looser: null }
       const finalPair = { winner: null, looser: null }
@@ -72,10 +86,10 @@ module.exports = function (router, models) {
         secondPair.looser = null
       }
 
-      models.BattleTable.create({
+      await models.BattleTable.create({
         eventSlug,
         turnNumber,
-        cellName: currentCell,
+        cellName: cell,
         players: playerList.map(player => {
           const { username, clanName, clanTag, avatar } = users.find(user => user.username === player)
           return {
