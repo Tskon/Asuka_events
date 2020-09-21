0f1f6acefff747b5fed7a928ef5be05473e82424
Tskon <tsekhmeister.k@gmail.com>
2020-09-21 22:09:09 +0300

    fix styles add event info

--- src/modules/HomePage.vue
+++ src/modules/HomePage.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
     <h1>Океаническая глобальная карта</h1>
-    <p class="greetings">{{ greetings }}</p>
+    <p class="greetings">
+      {{ greetings }}
+    </p>
     <div v-if="isAuth">
       Чтобы зааписаться в новый эвент обратись к Тактику.
       <template v-if="userEventList.length">
-        Ты участвуешь в эвентах: {{ userEventList.join(', ') }}
+        <ul class="event-list">
+          Ты участвуешь в эвентах:
+          <li
+            v-for="event in userEventList"
+            :key="event.slug"
+          >
+            • {{ event.name }}
+            (ход: {{ event.currentTurn.turnNumber }}, счет: {{ event.score }})
+          </li>
+        </ul>
       </template>
       <template>
         Сейчас ты не участвуешь в эвентах.
@@ -37,7 +48,10 @@ export default {
     userEventList() {
       return this.playerEvents.map(playerEvent => {
         const fullEvent = this.events.find(event => event.slug === playerEvent.slug)
-        return fullEvent.name
+        return {
+          ...fullEvent,
+          ...playerEvent
+        }
       })
     }
   }
@@ -50,7 +64,6 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  text-align: center;
   margin: auto;
   max-width: 600px;
   height: calc(100% - 56px);
@@ -58,5 +71,17 @@ export default {
 
 .greetings {
   font-size: 24px;
+  margin: 0 auto;
+}
+
+.event-list {
+  width: 250px;
+  text-align: left;
+  list-style: none;
+  padding: 12px 0 0;
+
+  li {
+    padding-left: 2px;
+  }
 }
 </style>
